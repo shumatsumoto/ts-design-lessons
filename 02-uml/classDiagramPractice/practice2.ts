bfa1af228346cca75c04eca5e0c51147d986cd57
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-04 13:22:45 +0900

    class 演習

--- 02-uml/classDiagramPractice/practice2.ts
+++ 02-uml/classDiagramPractice/practice2.ts
@@ -1,26 +1,26 @@
 // クラス図 演習2
 
 interface Shape {
-    calcArea(): number;
+  calcArea(): number;
 }
 
 class Rectangle implements Shape {
-    private width: number;
-    private height: number;
+  private width: number;
+  private height: number;
 
-    calcArea(): number {
-        return this.width * this.height;
-    }
+  calcArea(): number {
+    return this.width * this.height;
+  }
 }
 
 class Circle implements Shape {
-    private radius: number;
+  private radius: number;
 
-    calcArea(): number {
-        return this.radius * this.radius * Math.PI;
-    }
+  calcArea(): number {
+    return this.radius * this.radius * Math.PI;
+  }
 }
 
 class Client {
-    private shape: Shape;
+  private shape: Shape;
 }
