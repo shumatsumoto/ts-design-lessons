be1bc7da61ac724c1de9f62d0c55729faed25f15
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-05 23:16:53 +0900

    単一責任の原則

--- 03-solid-principle/singleResponsibility/goodExample.ts
+++ 03-solid-principle/singleResponsibility/goodExample.ts
@@ -1,46 +1,43 @@
 class EmployeeData {
-    constructor(
-        public name: string,
-        public department: string,
-    ) {}
+  constructor(public name: string, public department: string) {}
 }
 
 class PayCalculator {
-    private getRegularHours() {
-        console.log("給与計算用の労働時間計算ロジック");
-    }
+  private getRegularHours() {
+    console.log("給与計算用の労働時間計算ロジック");
+  }
 
-    calculatePay(employeeData: EmployeeData) {
-        this.getRegularHours();
-        console.log(`${employeeData.name}の給与を計算しました`);
-    }
+  calculatePay(employeeData: EmployeeData) {
+    this.getRegularHours();
+    console.log(`${employeeData.name}の給与を計算しました`);
+  }
 }
 
 class HourReporter {
-    private getRegularHours() {
-        console.log("労働時間レポート専用の労働時間計算ロジック");
-    }
+  private getRegularHours() {
+    console.log("労働時間レポート専用の労働時間計算ロジック");
+  }
 
-    reportHours(employeeData: EmployeeData) {
-        this.getRegularHours();
-        console.log(`${employeeData.name}の労働時間をレポートしました`);
-    }
+  reportHours(employeeData: EmployeeData) {
+    this.getRegularHours();
+    console.log(`${employeeData.name}の労働時間をレポートしました`);
+  }
 }
 
 class EmployeeRepository {
-    save() {}
+  save() {}
 }
 
 function run() {
-    const employeeData = new EmployeeData("鈴木", "開発");
-    const payCalculator = new PayCalculator();
-    const hourReporter = new HourReporter();
+  const employeeData = new EmployeeData("鈴木", "開発");
+  const payCalculator = new PayCalculator();
+  const hourReporter = new HourReporter();
 
-    console.log("経理部門");
-    payCalculator.calculatePay(employeeData);
-    console.log("");
-    console.log("人事部門");
-    hourReporter.reportHours(employeeData);
+  console.log("経理部門");
+  payCalculator.calculatePay(employeeData);
+  console.log("");
+  console.log("人事部門");
+  hourReporter.reportHours(employeeData);
 }
 
 run();
