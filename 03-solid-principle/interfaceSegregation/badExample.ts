080957e23068ac03a672a5208081926c5c20d7e0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-06 10:17:41 +0900

    インターフェイス分離の原則

--- 03-solid-principle/interfaceSegregation/badExample.ts
+++ 03-solid-principle/interfaceSegregation/badExample.ts
@@ -1,54 +1,46 @@
-export {}
+export {};
 
 interface Vehicle {
-    name: string;
-    color: string;
-    start(): void;
-    stop(): void;
-    fly(): void;
+  name: string;
+  color: string;
+  start(): void;
+  stop(): void;
+  fly(): void;
 }
 
 class Airplane implements Vehicle {
-    constructor(
-        public name: string, 
-        public color: string
-    ) {
-
-    }
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
-    fly() {
-        console.log("fly!");
-    }
+  constructor(public name: string, public color: string) {}
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
+  fly() {
+    console.log("fly!");
+  }
 }
 
 class Car implements Vehicle {
-    constructor(
-        public name: string,
-        public color: string
-    ) {}
+  constructor(public name: string, public color: string) {}
 
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
-    fly() {
-        throw new Error("車は空を飛べません");
-    }
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
+  fly() {
+    throw new Error("車は空を飛べません");
+  }
 }
 
 function run() {
-    const v1: Vehicle = new Airplane("AirBus", "white");
-    const v2: Vehicle = new Car("Prius", "black");
+  const v1: Vehicle = new Airplane("AirBus", "white");
+  const v2: Vehicle = new Car("Prius", "black");
 
-    v1.fly();
-    v2.fly();
+  v1.fly();
+  v2.fly();
 }
 
 run();
--- 03-solid-principle/interfaceSegregation/goodExample.ts
+++ 03-solid-principle/interfaceSegregation/goodExample.ts
@@ -1,57 +1,49 @@
-export {}
+export {};
 
 interface Vehicle {
-    name: string;
-    color: string;
+  name: string;
+  color: string;
 }
 
 interface Movable {
-    start();
-    stop();
+  start();
+  stop();
 }
 
 interface Flyable {
-    fly();
+  fly();
 }
 
 class Airplane implements Vehicle, Movable, Flyable {
-    constructor(
-        public name: string, 
-        public color: string
-    ) {
-
-    }
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
-    fly() {
-        console.log("fly!");
-    }
+  constructor(public name: string, public color: string) {}
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
+  fly() {
+    console.log("fly!");
+  }
 }
 
 class Car implements Vehicle, Movable {
-    constructor(
-        public name: string,
-        public color: string
-    ) {}
+  constructor(public name: string, public color: string) {}
 
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
 }
 
 function run() {
-    const v1 = new Airplane("AirBus", "white");
-    const v2 = new Car("Prius", "black");
+  const v1 = new Airplane("AirBus", "white");
+  const v2 = new Car("Prius", "black");
 
-    v1.fly();
-    v2.start();
+  v1.fly();
+  v2.start();
 }
 
 run();
