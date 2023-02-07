5cf5b2f18f78d553aed66a4292313cb4d7df3f86
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-07 14:28:26 +0900

    依存性逆転の原則

--- 03-solid-principle/dependencyInversion/badExample.ts
+++ 03-solid-principle/dependencyInversion/badExample.ts
@@ -1,46 +1,46 @@
-export {}
+export {};
 
 class User {}
 
 class UserController {
-    private userService = new UserService();
+  private userService = new UserService();
 
-    create(user: User): User {
-        return this.userService.create(user);
-    }
+  create(user: User): User {
+    return this.userService.create(user);
+  }
 
-    findById(id: string): User {
-        return this.userService.findById(id);
-    }
+  findById(id: string): User {
+    return this.userService.findById(id);
+  }
 }
 
 class UserService {
-    private userRepository = new UserRdbRepository();
+  private userRepository = new UserRdbRepository();
 
-    create(user: User): User {
-        return this.userRepository.create(user);
-    }
+  create(user: User): User {
+    return this.userRepository.create(user);
+  }
 
-    findById(id: string): User {
-        return this.userRepository.findById(id);
-    }
+  findById(id: string): User {
+    return this.userRepository.findById(id);
+  }
 }
 
 class UserRdbRepository {
-    create(user: User): User {
-        console.log("RDBにUserを登録");
-        return user;
-    }
+  create(user: User): User {
+    console.log("RDBにUserを登録");
+    return user;
+  }
 
-    findById(id: string): User {
-        console.log(`ID: ${id}のユーザーを検索`);
-        return new User();
-    }
+  findById(id: string): User {
+    console.log(`ID: ${id}のユーザーを検索`);
+    return new User();
+  }
 }
 
 function run() {
-    const userController = new UserController();
-    userController.findById("123");
+  const userController = new UserController();
+  userController.findById("123");
 }
 
 run();
