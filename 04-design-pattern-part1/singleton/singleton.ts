5e3f3b99607c83d154025c4c548b9552e4f0540e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-07 15:56:40 +0900

    Singleton

--- 04-design-pattern-part1/singleton/singleton.ts
+++ 04-design-pattern-part1/singleton/singleton.ts
@@ -1,37 +1,37 @@
-export {}
+export {};
 
 class Logger {
-    private static instance: Logger;
+  private static instance: Logger;
 
-    private constructor() {}
+  private constructor() {}
 
-    static getInstance() {
-        if (!this.instance) {
-            this.instance = new Logger();
-        }
-
-        return this.instance;
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Logger();
     }
 
-    output(content: string) {
-        const now = new Date();
-        console.log(`${now.toLocaleString('ja-JP')}: ${content}`);
-    }
+    return this.instance;
+  }
+
+  output(content: string) {
+    const now = new Date();
+    console.log(`${now.toLocaleString("ja-JP")}: ${content}`);
+  }
 }
 
 class Test {}
 
 function run() {
-    const test1 = new Test();
-    const test2 = new Test();
-    console.log('Testクラス: ', test1 === test2);
+  const test1 = new Test();
+  const test2 = new Test();
+  console.log("Testクラス: ", test1 === test2);
 
-    const logger1 = Logger.getInstance();
-    const logger2 = Logger.getInstance();
-    console.log('Loggerクラス: ', logger1 === logger2);
+  const logger1 = Logger.getInstance();
+  const logger2 = Logger.getInstance();
+  console.log("Loggerクラス: ", logger1 === logger2);
 
-    logger1.output('logger1のログ');
-    logger2.output('logger2のログ');
+  logger1.output("logger1のログ");
+  logger2.output("logger2のログ");
 }
 
 run();
