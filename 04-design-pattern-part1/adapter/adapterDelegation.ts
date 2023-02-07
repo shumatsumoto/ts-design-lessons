890f280bb4672a0ad9cf3584a37720f2e367a70d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-07 16:07:42 +0900

    Adapter

--- 04-design-pattern-part1/adapter/adapterDelegation.ts
+++ 04-design-pattern-part1/adapter/adapterDelegation.ts
@@ -1,49 +1,53 @@
-export {}
+export {};
 
 interface Target {
-    getCsvData(): string;
+  getCsvData(): string;
 }
 
 class NewLibrary {
-    getJsonData() {
-        return [
-            {
-                "data1": "json_dataA",
-                "data2": "json_dataB",
-            },
-            {
-                "data1": "json_dataC",
-                "data2": "json_dataD",
-            }
-        ];
-    }
+  getJsonData() {
+    return [
+      {
+        data1: "json_dataA",
+        data2: "json_dataB",
+      },
+      {
+        data1: "json_dataC",
+        data2: "json_dataD",
+      },
+    ];
+  }
 }
 
 class JsonToCsvAdapter implements Target {
-    constructor(private adaptee: NewLibrary) {}
+  constructor(private adaptee: NewLibrary) {}
 
-    getCsvData(): string {
-        const jsonData = this.adaptee.getJsonData();
+  getCsvData(): string {
+    const jsonData = this.adaptee.getJsonData();
 
-        const header = Object.keys(jsonData[0]).join(',') + '\n';
-        const body = jsonData.map(d => {
-            return Object.keys(d).map(key => d[key]).join(',');
-        }).join('\n');
+    const header = Object.keys(jsonData[0]).join(",") + "\n";
+    const body = jsonData
+      .map((d) => {
+        return Object.keys(d)
+          .map((key) => d[key])
+          .join(",");
+      })
+      .join("\n");
 
-        return header + body;
-    }
+    return header + body;
+  }
 }
 
 function run() {
-    const adaptee = new NewLibrary();
-    console.log('=== Adapteeが提供するデータ ===');
-    console.log(adaptee.getJsonData());
+  const adaptee = new NewLibrary();
+  console.log("=== Adapteeが提供するデータ ===");
+  console.log(adaptee.getJsonData());
 
-    console.log("");
+  console.log("");
 
-    const adapter = new JsonToCsvAdapter(adaptee);
-    console.log('=== Adapterに変換されたデータ ===');
-    console.log(adapter.getCsvData());
+  const adapter = new JsonToCsvAdapter(adaptee);
+  console.log("=== Adapterに変換されたデータ ===");
+  console.log(adapter.getCsvData());
 }
 
 run();
--- 04-design-pattern-part1/adapter/adapterInheritance.ts
+++ 04-design-pattern-part1/adapter/adapterInheritance.ts
@@ -1,47 +1,51 @@
-export {}
+export {};
 
 interface Target {
-    getCsvData(): string;
+  getCsvData(): string;
 }
 
 class NewLibrary {
-    getJsonData() {
-        return [
-            {
-                "data1": "json_dataA",
-                "data2": "json_dataB",
-            },
-            {
-                "data1": "json_dataC",
-                "data2": "json_dataD",
-            }
-        ];
-    }
+  getJsonData() {
+    return [
+      {
+        data1: "json_dataA",
+        data2: "json_dataB",
+      },
+      {
+        data1: "json_dataC",
+        data2: "json_dataD",
+      },
+    ];
+  }
 }
 
 class JsonToCsvAdapter extends NewLibrary implements Target {
-    getCsvData(): string {
-        const jsonData = this.getJsonData();
+  getCsvData(): string {
+    const jsonData = this.getJsonData();
 
-        const header = Object.keys(jsonData[0]).join(',') + '\n';
-        const body = jsonData.map(d => {
-            return Object.keys(d).map(key => d[key]).join(',');
-        }).join('\n');
+    const header = Object.keys(jsonData[0]).join(",") + "\n";
+    const body = jsonData
+      .map((d) => {
+        return Object.keys(d)
+          .map((key) => d[key])
+          .join(",");
+      })
+      .join("\n");
 
-        return header + body;
-    }
+    return header + body;
+  }
 }
 
 function run() {
-    const adaptee = new NewLibrary();
-    console.log('=== Adapteeが提供するデータ ===');
-    console.log(adaptee.getJsonData());
+  const adaptee = new NewLibrary();
+  console.log("=== Adapteeが提供するデータ ===");
+  console.log(adaptee.getJsonData());
 
-    console.log("");
+  console.log("");
 
-    const adapter = new JsonToCsvAdapter();
-    console.log('=== Adapterに変換されたデータ ===');
-    console.log(adapter.getCsvData());
+  const adapter = new JsonToCsvAdapter();
+  console.log("=== Adapterに変換されたデータ ===");
+  console.log(adapter.getCsvData());
 }
 
 run();
