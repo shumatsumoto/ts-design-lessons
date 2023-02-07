f3d320c2eaa548e5be6816e34f9a27e99b72564a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-07 14:51:08 +0900

    Template method

--- 04-design-pattern-part1/template_method/templateMethod.ts
+++ 04-design-pattern-part1/template_method/templateMethod.ts
@@ -1,47 +1,47 @@
-export {}
+export {};
 
 abstract class TestTemplate {
-    test() {
-        this.setup();
-        this.execute();
-        this.teardown();
-    }
+  test() {
+    this.setup();
+    this.execute();
+    this.teardown();
+  }
 
-    abstract setup();
-    abstract execute();
+  abstract setup();
+  abstract execute();
 
-    teardown() {
-        console.log("teardown");
-    }
+  teardown() {
+    console.log("teardown");
+  }
 }
 
 class ItemServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: ItemServiceTest");
-    }
+  setup() {
+    console.log("setup: ItemServiceTest");
+  }
 
-    execute() {
-        console.log("execute: ItemServiceTest");
-    }
+  execute() {
+    console.log("execute: ItemServiceTest");
+  }
 }
 
 class UserServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: UserServiceTest");
-    }
+  setup() {
+    console.log("setup: UserServiceTest");
+  }
 
-    execute() {
-        console.log("execute: UserServiceTest");
-    }
+  execute() {
+    console.log("execute: UserServiceTest");
+  }
 }
 
 function run() {
-    const itemServiceTest = new ItemServiceTest();
-    const userServiceTest = new UserServiceTest();
+  const itemServiceTest = new ItemServiceTest();
+  const userServiceTest = new UserServiceTest();
 
-    itemServiceTest.test();
-    console.log("");
-    userServiceTest.test();
+  itemServiceTest.test();
+  console.log("");
+  userServiceTest.test();
 }
 
 run();
