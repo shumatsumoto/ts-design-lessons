723091286135623f4344ea33a1985c8ccfcadf7e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-13 10:21:07 +0900

    Prototype

--- 05-design-pattern-part2/prototype/prototype.ts
+++ 05-design-pattern-part2/prototype/prototype.ts
@@ -1,76 +1,73 @@
-export {}
+export {};
 
 import _ from "lodash";
 
 abstract class ItemPrototype {
-    constructor(
-        public name: string,
-        public detail: Detail = {"comment": []}
-    ) {}
+  constructor(public name: string, public detail: Detail = { comment: [] }) {}
 
-    addComment(comment: string) {
-        this.detail.comment.push(comment);
-    }
+  addComment(comment: string) {
+    this.detail.comment.push(comment);
+  }
 
-    abstract createCopy(): ItemPrototype;
+  abstract createCopy(): ItemPrototype;
 }
 
-type Detail = {"comment": string[]};
+type Detail = { comment: string[] };
 
 class DeepCopyItem extends ItemPrototype {
-    createCopy(): ItemPrototype {
-        return _.cloneDeep(this);
-    }
+  createCopy(): ItemPrototype {
+    return _.cloneDeep(this);
+  }
 }
 
 class ShallowCopyItem extends ItemPrototype {
-    createCopy(): ItemPrototype {
-        return _.clone(this);
-    }
+  createCopy(): ItemPrototype {
+    return _.clone(this);
+  }
 }
 
 class ItemManager {
-    items: {[key: string]: ItemPrototype} = {};
+  items: { [key: string]: ItemPrototype } = {};
 
-    registerItem(key: string, item: ItemPrototype) {
-        this.items[key] = item;
-    }
+  registerItem(key: string, item: ItemPrototype) {
+    this.items[key] = item;
+  }
 
-    create(key: string) {
-        if (key in this.items) {
-            const item = this.items[key];
-            return item.createCopy();
-        }
-        throw new Error("指定されたキーは存在しません");
+  create(key: string) {
+    if (key in this.items) {
+      const item = this.items[key];
+      return item.createCopy();
     }
+    throw new Error("指定されたキーは存在しません");
+  }
 }
 
 function run() {
-    const mouse = new DeepCopyItem("マウス");
-    mouse.addComment("original");
+  const mouse = new DeepCopyItem("マウス");
+  mouse.addComment("original");
 
-    const keyboard = new ShallowCopyItem("キーボード");
-    keyboard.addComment("original");
+  const keyboard = new ShallowCopyItem("キーボード");
+  keyboard.addComment("original");
 
-    const manager = new ItemManager();
-    manager.registerItem("mouse", mouse);
-    manager.registerItem("keyboard", keyboard);
+  const manager = new ItemManager();
+  manager.registerItem("mouse", mouse);
+  manager.registerItem("keyboard", keyboard);
 
-    const clonedMouse = manager.create("mouse");
-    const clonedKeyboard = manager.create("keyboard");
+  const clonedMouse = manager.create("mouse");
+  const clonedKeyboard = manager.create("keyboard");
 
-    console.log("マウス（オリジナル）: ", mouse);
-    console.log("マウス（コピー）: ", clonedMouse);
-    console.log("キーボード（オリジナル）: ", keyboard);
-    console.log("キーボード（コピー）: ", clonedKeyboard);
+  console.log("マウス（オリジナル）: ", mouse);
+  console.log("マウス（コピー）: ", clonedMouse);
+  console.log("キーボード（オリジナル）: ", keyboard);
+  console.log("キーボード（コピー）: ", clonedKeyboard);
 
-    clonedMouse.addComment("Good");
-    clonedKeyboard.addComment("SoSo");
-    console.log("");
-    console.log("マウス（オリジナル）: ", mouse);
-    console.log("マウス（コピー）: ", clonedMouse);
-    console.log("キーボード（オリジナル）: ", keyboard);
-    console.log("キーボード（コピー）: ", clonedKeyboard);
+  clonedMouse.addComment("Good");
+  clonedKeyboard.addComment("SoSo");
+  console.log("");
+  console.log("マウス（オリジナル）: ", mouse);
+  console.log("マウス（コピー）: ", clonedMouse);
+  console.log("キーボード（オリジナル）: ", keyboard);
+  console.log("キーボード（コピー）: ", clonedKeyboard);
 }
 
 run();
