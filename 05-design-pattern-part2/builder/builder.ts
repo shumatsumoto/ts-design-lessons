d64c3f697fc6e1de54553bfcd23975a6b8362353
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-13 11:35:41 +0900

    Builder

--- 05-design-pattern-part2/builder/builder.ts
+++ 05-design-pattern-part2/builder/builder.ts
@@ -1,84 +1,84 @@
 export {};
 
 class Computer {
-    type: string;
-    cpu: string;
-    ram: number;
+  type: string;
+  cpu: string;
+  ram: number;
 }
 
 interface ComputerBuilder {
-    addCpu(cpu: string);
-    addRam(ram: number);
+  addCpu(cpu: string);
+  addRam(ram: number);
 }
 
 class DesktopBuilder implements ComputerBuilder {
-    private computer: Computer;
+  private computer: Computer;
 
-    constructor() {
-        this.computer = new Computer();
-        this.computer.type = "Desktop";
-    }
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = "Desktop";
+  }
 
-    addCpu(cpu: string) {
-        this.computer.cpu = cpu;
-    }
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
 
-    addRam(ram: number) {
-        this.computer.ram = ram;
-    }
+  addRam(ram: number) {
+    this.computer.ram = ram;
+  }
 
-    getResult(): Computer {
-        return this.computer;
-    }
+  getResult(): Computer {
+    return this.computer;
+  }
 }
 
 class LaptopBuilder implements ComputerBuilder {
-    private computer: Computer;
+  private computer: Computer;
 
-    constructor() {
-        this.computer = new Computer();
-        this.computer.type = "Laptop";
-    }
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = "Laptop";
+  }
 
-    addCpu(cpu: string) {
-        this.computer.cpu = cpu;
-    }
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
 
-    addRam(ram: number) {
-        this.computer.ram = ram;
-    }
+  addRam(ram: number) {
+    this.computer.ram = ram;
+  }
 
-    getResult(): Computer {
-        return this.computer;
-    }
+  getResult(): Computer {
+    return this.computer;
+  }
 }
 
 class Director {
-    constructor(private builder: ComputerBuilder) {}
+  constructor(private builder: ComputerBuilder) {}
 
-    construct() {
-        this.builder.addCpu("Core i5");
-        this.builder.addRam(16);
-    }
+  construct() {
+    this.builder.addCpu("Core i5");
+    this.builder.addRam(16);
+  }
 
-    highSpecConstruct() {
-        this.builder.addCpu("Core i9");
-        this.builder.addRam(64);
-    }
+  highSpecConstruct() {
+    this.builder.addCpu("Core i9");
+    this.builder.addRam(64);
+  }
 }
 
 function run() {
-    const desktopBuilder = new DesktopBuilder();
-    const desktopDirector = new Director(desktopBuilder);
-    desktopDirector.construct();
-    const desktopComputer = desktopBuilder.getResult();
-    console.log(desktopComputer);
+  const desktopBuilder = new DesktopBuilder();
+  const desktopDirector = new Director(desktopBuilder);
+  desktopDirector.construct();
+  const desktopComputer = desktopBuilder.getResult();
+  console.log(desktopComputer);
 
-    const laptopBuilder = new LaptopBuilder();
-    const laptopDirector = new Director(laptopBuilder);
-    laptopDirector.highSpecConstruct();
-    const laptopComputer = laptopBuilder.getResult();
-    console.log(laptopComputer);
+  const laptopBuilder = new LaptopBuilder();
+  const laptopDirector = new Director(laptopBuilder);
+  laptopDirector.highSpecConstruct();
+  const laptopComputer = laptopBuilder.getResult();
+  console.log(laptopComputer);
 }
 
 run();
