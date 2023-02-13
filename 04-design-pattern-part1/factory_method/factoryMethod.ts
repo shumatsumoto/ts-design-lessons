31a5ba84c30c92e45fbdfb5bab2afd82150f38d9
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-13 09:32:36 +0900

    FactoryMethod

--- 04-design-pattern-part1/factory_method/factoryMethod.ts
+++ 04-design-pattern-part1/factory_method/factoryMethod.ts
@@ -1,79 +1,79 @@
-export {}
+export {};
 
 abstract class CreditCard {
-    constructor(public owner: string) {}
+  constructor(public owner: string) {}
 
-    getOwner(): string {
-        return this.owner;
-    }
+  getOwner(): string {
+    return this.owner;
+  }
 
-    abstract getCardType(): string;
-    abstract getAnnualCharge(): number;
+  abstract getCardType(): string;
+  abstract getAnnualCharge(): number;
 }
 
 class Platinum extends CreditCard {
-    getCardType(): string {
-        return "Platinum";
-    }
+  getCardType(): string {
+    return "Platinum";
+  }
 
-    getAnnualCharge(): number {
-        return 30000;
-    }
+  getAnnualCharge(): number {
+    return 30000;
+  }
 }
 
 class Gold extends CreditCard {
-    getCardType(): string {
-        return "Gold";
-    }
+  getCardType(): string {
+    return "Gold";
+  }
 
-    getAnnualCharge(): number {
-        return 10000;
-    }
+  getAnnualCharge(): number {
+    return 10000;
+  }
 }
 
 abstract class CreditCardFactory {
-    abstract createCreditCard(owner: string): CreditCard;
-    abstract registerCreditCard(creditCard: CreditCard);
+  abstract createCreditCard(owner: string): CreditCard;
+  abstract registerCreditCard(creditCard: CreditCard);
 
-    create(owner: string): CreditCard {
-        const creditCard = this.createCreditCard(owner);
-        this.registerCreditCard(creditCard);
-        return creditCard;
-    }
+  create(owner: string): CreditCard {
+    const creditCard = this.createCreditCard(owner);
+    this.registerCreditCard(creditCard);
+    return creditCard;
+  }
 }
 
 const creditCardDatabase = [];
 
 class PlatinumCreditCardFactory extends CreditCardFactory {
-    createCreditCard(owner: string): CreditCard {
-        return new Platinum(owner);
-    }
+  createCreditCard(owner: string): CreditCard {
+    return new Platinum(owner);
+  }
 
-    registerCreditCard(creditCard: CreditCard) {
-        creditCardDatabase.push(creditCard);
-    }
+  registerCreditCard(creditCard: CreditCard) {
+    creditCardDatabase.push(creditCard);
+  }
 }
 
 class GoldCreditCardFactory extends CreditCardFactory {
-    createCreditCard(owner: string): CreditCard {
-        return new Gold(owner);
-    }
+  createCreditCard(owner: string): CreditCard {
+    return new Gold(owner);
+  }
 
-    registerCreditCard(creditCard: CreditCard) {
-        creditCardDatabase.push(creditCard);
-    }
+  registerCreditCard(creditCard: CreditCard) {
+    creditCardDatabase.push(creditCard);
+  }
 }
 
 function run() {
-    const platinumCardFactory = new PlatinumCreditCardFactory();
-    const platinumCard = platinumCardFactory.create("Tanaka");
-    console.log(platinumCard);
+  const platinumCardFactory = new PlatinumCreditCardFactory();
+  const platinumCard = platinumCardFactory.create("Tanaka");
+  console.log(platinumCard);
 
-    const goldCreditCardFactory = new GoldCreditCardFactory();
-    const goldCard = goldCreditCardFactory.create("Suzuki");
-    console.log(goldCard);
+  const goldCreditCardFactory = new GoldCreditCardFactory();
+  const goldCard = goldCreditCardFactory.create("Suzuki");
+  console.log(goldCard);
 
-    console.log(creditCardDatabase);
+  console.log(creditCardDatabase);
 }
 
 run();
