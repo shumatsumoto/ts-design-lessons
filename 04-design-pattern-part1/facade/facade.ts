1a6c2da85e39ed77e17015149d13bd80044e606a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-13 09:52:44 +0900

    facade

--- 04-design-pattern-part1/facade/facade.ts
+++ 04-design-pattern-part1/facade/facade.ts
@@ -1,44 +1,44 @@
-export {}
+export {};
 
 class Product {
-    getProduct(name: string) {
-        console.log(`${name}を取得しました`);
-    }
+  getProduct(name: string) {
+    console.log(`${name}を取得しました`);
+  }
 }
 
 class Payment {
-    makePayment(name: string) {
-        console.log(`${name}の支払いが完了しました`);
-    }
+  makePayment(name: string) {
+    console.log(`${name}の支払いが完了しました`);
+  }
 }
 
 class Invoice {
-    sendInvoice(name: string) {
-        console.log(`${name}の請求書が送信されました`);
-    }
+  sendInvoice(name: string) {
+    console.log(`${name}の請求書が送信されました`);
+  }
 }
 
 class Order {
-    placeOrder(name: string) {
-        console.log("注文開始");
+  placeOrder(name: string) {
+    console.log("注文開始");
 
-        const product = new Product();
-        product.getProduct(name);
+    const product = new Product();
+    product.getProduct(name);
 
-        const payment = new Payment();
-        payment.makePayment(name);
+    const payment = new Payment();
+    payment.makePayment(name);
 
-        const invoice = new Invoice();
-        invoice.sendInvoice(name);
+    const invoice = new Invoice();
+    invoice.sendInvoice(name);
 
-        console.log("注文は正常に完了しました");
-    }
+    console.log("注文は正常に完了しました");
+  }
 }
 
 function run() {
-    const name = "デザインパターン本";
-    const order = new Order();
-    order.placeOrder(name);
+  const name = "デザインパターン本";
+  const order = new Order();
+  order.placeOrder(name);
 }
 
 run();
